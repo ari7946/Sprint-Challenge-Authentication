c5da122f8535bac26335a97d4340628402198709
Ari Mercado <ari@Apples-Mac-3.local>
2018-08-10 11:49:08 -0700

    removed unnecessary code

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const { server } = require('./server');
-// const express = require('express');
-// const cors = require('cors');
 
 const port = process.env.PORT || 5000;
-
-server.use(express.json());
-server.use(cors({origin: 'http://localhost:3000', credentials:true}));
 
 server.listen(port, () => {
   console.log(`\n=== Server listening on port ${port}\n`);
